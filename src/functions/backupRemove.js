518752934b775e6279d815b4edcad38d4dc46744
tyowk <tyowk.personal@gmail.com>
2024-06-26 21:51:18 +0700

    Update backupRemove.js

--- src/functions/backupRemove.js
+++ src/functions/backupRemove.js
@@ -8,16 +8,12 @@ module.exports = class BackupRemoveFunc {
     async execute(d) {
         const data = d.util.aoiFunc(d);
         if (data.err) return d.error(data.err);
-        
         try {
             const [ backupId ] = data.inside.splits;
-            if (!backupId) return throw new Error('No Backup ID Provided In');
-
+            if (!backupId) throw new Error('No Backup ID Provided');
             await this.backup.remove(backupId);
             data.result = null;
-            return {
-                code: d.util.setCode(data),
-            };
+            return { code: d.util.setCode(data) };
         } catch (err) {
             return d.aoiError.fnError(d, 'custom', {}, `${err.name}: ${err.message}`);
         }
