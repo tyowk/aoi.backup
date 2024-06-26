c11c5bd8d8b0b69278bf9e41f64bc1de60cf7979
tyowk <tyowk.personal@gmail.com>
2024-06-26 21:49:17 +0700

    Update backupList.js

--- src/functions/backupList.js
+++ src/functions/backupList.js
@@ -8,15 +8,11 @@ module.exports = class BackupListFunc {
     async execute(d) {
         const data = d.util.aoiFunc(d);
         if (data.err) return d.error(data.err);
-
         try {
-            const [ separator = ', '] = data.inside.splits;
+            const [ separator = ', ' ] = data.inside.splits;
             const backupList = this.backup.list();
-
             data.result = backupList.join(separator);
-            return {
-                code: d.util.setCode(data),
-            };
+            return { code: d.util.setCode(data) };
         } catch (err) {
             return d.aoiError.fnError(d, 'custom', {}, `${err.name}: ${err.message}`);
         }
