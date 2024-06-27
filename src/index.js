34c5cd31583a36857432d0f84954bfc3caeb1554
tyowk <tyowk.personal@gmail.com>
2024-06-27 13:41:11 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,40 +5,34 @@ const packageJson = require('../package.json');
 
 class AoiBackup {
     constructor(client, basePath = './backups') {
-        (async () => {
-            try {
-                if (!fs.existsSync(basePath)) {
-                    fs.mkdirSync(basePath);
-                }
-                backup.setStorageFolder(path.join(process.cwd(), basePath));
-                const files = await fs.promises.readdir(path.join(__dirname, 'functions'));
-                for (const file of files) {
-                    const FunctionClass = require(`./functions/${file}`);
-                    const funcData = new FunctionClass(backup);
-                    client.functionManager.createFunction({
-                        name: funcData.name,
-                        type: funcData.type,
-                        code: funcData.execute.bind(funcData)
-                    });
-                }
-                console.log('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[92mFunctions loaded successfully!\x1b[0m');
-            } catch (err) {
-                console.error('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[91mAn error occurred: \x1b[0m', err);
-            }
+        try {
+            if (!fs.existsSync(basePath)) fs.mkdirSync(basePath);
+            backup.setStorageFolder(path.join(process.cwd(), basePath));
 
-            try {
-                const res = await fetch(`https://registry.npmjs.org/${packageJson.name}`, { headers: { "User-Agent": packageJson.name } }).then(res => res.json());
-                if (!res.versions[packageJson.version]) {
-                    console.warn('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[93mThis is a dev version. Some stuff may be incomplete or unstable\x1b[0m');
-                    return;
-                }
-                if (packageJson.version !== res["dist-tags"].latest) {
-                    console.warn('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[93mAoi.backup is outdated. Update to the latest version!\x1b[0m');
-                }
-            } catch (err) {
-                console.error('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[91mAn error occurred: \x1b[0m', err);
-            }
-        })();
+            ['create', 'load', 'fetch', 'remove', 'list'].forEach(file => {
+                const FuncClass = require(`./functions/${file}`);
+                const funcData = new FuncClass(backup);
+                client.functionManager.createFunction({
+                    name: funcData.name,
+                    type: funcData.type,
+                    code: funcData.execute.bind(funcData)
+                });
+            });
+
+            console.log('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[92mFunctions loaded successfully!\x1b[0m');
+        } catch (err) {
+            console.error('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[91mAn error occurred:\x1b[0m', err);
+        }
+
+        fetch(`https://registry.npmjs.org/${packageJson.name}`, { headers: { "User-Agent": packageJson.name } })
+            .then(res => res.json())
+            .then(data => {
+                if (!data.versions[packageJson.version]) console.warn('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[93mThis is a dev version. Some stuff may be incomplete or unstable\x1b[0m');
+                if (packageJson.version !== data["dist-tags"].latest) console.warn('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[93mAoi.backup is outdated. Update to the latest version!\x1b[0m');
+            })
+            .catch(err => {
+                console.error('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[91mAn error occurred:\x1b[0m', err);
+            });
     }
 }
 
