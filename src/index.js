fbc5f58a2b44f232a2670480c46b63a007aa68cf
tyowk <tyowk.personal@gmail.com>
2024-06-27 09:16:27 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,20 +23,20 @@ class AoiBackup {
                 }
                 console.log('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[92mFunctions loaded successfully!\x1b[0m');
             } catch (err) {
-                console.log('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[91mAn error occurred: \x1b[0m', err);
+                console.error('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[91mAn error occurred: \x1b[0m', err);
             }
 
             try {
                 const res = await fetch(`https://registry.npmjs.org/${packageJson.name}`, { headers: { "User-Agent": packageJson.name } }).then(res => res.json());
                 if (!res.versions[packageJson.version]) {
-                    console.log('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[93mThis is a dev version. Some stuff may be incomplete or unstable\x1b[0m');
+                    console.warn('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[93mThis is a dev version. Some stuff may be incomplete or unstable\x1b[0m');
                     return;
                 }
                 if (packageJson.version !== res["dist-tags"].latest) {
-                    console.log('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[93mAoi.backup is outdated. Update to latest version!\x1b[0m');
+                    console.warn('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[93mAoi.backup is outdated. Update to the latest version!\x1b[0m');
                 }
             } catch (err) {
-                console.log('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[91mAn error occurred: \x1b[0m', err);
+                console.error('\x1b[1m[\x1b[96mAoi.backup\x1b[0m\x1b[1m] :: \x1b[91mAn error occurred: \x1b[0m', err);
             }
         })();
     }
